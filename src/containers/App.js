de3f199b0a34c19fb7496fef3db3ebb0c264cce6
connor <connoroakvillea32@hotmail.com>
2022-08-25 15:09:38 -0400

    cleaned up file directory

---
+++ src/containers/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+
+    constructor() {
+        super();
+    }
+
+    render() {
+        return(
+            <div className="App">
+
+            </div>
+        )
+    }
+
+}
+
+export default App;
